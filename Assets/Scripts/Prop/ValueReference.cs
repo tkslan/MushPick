beb1a494f5c0d0e4d412b3727f98117a51f4f184
Tomasz Slanda <tkslan@gmail.com>
2018-10-07 18:11:53 +0200

    Reorder in ValueReference

--- Assets/Scripts/Prop/ValueReference.cs
+++ Assets/Scripts/Prop/ValueReference.cs
@@ -7,7 +7,8 @@ public enum ReferenceType { Reference, Constant }
 [Serializable]
 public class ValueReference
 {
+    public float FloatVariable { get { return ReferenceType == ReferenceType.Constant ? Amount : Value.FloatVariable; } }
     public float Amount = 1;
     public Value Value;
     public ReferenceType ReferenceType;
-    public float FloatVariable { get { return ReferenceType == ReferenceType.Constant ? Amount : Value.FloatVariable; } }}
+}
